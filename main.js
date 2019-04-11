8dc66cc21cbefc1d658df7a1b73953e3d5252e7f
BenasdTW <ben6312tt@gmail.com>
2019-04-11 18:51:02 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow;
+
+function createWindow () {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // and load the index.html of the app.
+    mainWindow.loadFile('index.html');
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+    if(mainWindow === null) createWindow();
+});
